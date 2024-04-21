87ed0eeb646fc31277d625cc755f9269ebfcb9cb
Ben Matheson <mathesonben@gmail.com>
2024-04-21 09:46:03 -0800

    initial

---
+++ config.py
@@ -0,0 +1,5 @@
+JSONURL = 'https://services2.arcgis.com/Ce3DhLRthdwbHlfF/ArcGIS/rest/services/RoadClosures_2170ae77776e46b7b1a8859ad222fcf5_public/FeatureServer/1/query?where=1%3D1&outFields=OBJECTID,street,subtype,description,direction,starttime,endtime,type,reference&outSR=4326&returnZ=false&f=geojson' # arc gis url
+TZ = 'America/Anchorage'
+OUTPUT_CLOSURES_FILE = "muni_closures.json"
+DELAY = 60 * 2
+
